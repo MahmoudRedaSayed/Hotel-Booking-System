d1a25e6f0f7819883e6eb9e6e552b783226b64ab
MahmoudRedaSayed <anamahmoud10203040@gmail.com>
2022-09-22 16:11:39 +0200

    add the filter of the date

--- client/src/Screens/HomeScreen.js
+++ client/src/Screens/HomeScreen.js
@@ -10,15 +10,61 @@ import { DatePicker, Space } from "antd";
 const { RangePicker } = DatePicker;
 export default function HomeScreen() {
     const [fromdate, setfromdate] = useState('');
-    const [todate, settodate] = useState('');  
+    const [todate, settodate] = useState(''); 
+    const [duplicatehotes, setduplicatehotes] = useState([]); 
+    const [filterDate,setFilterDate]=useState(false);
+    const [hotels,setHotels]=useState([]);
     const {rooms,error,loading,success}=useSelector(state=>state.getAllRooms);
     const dispatch=useDispatch();
     useEffect(()=>{
         dispatch(getAllRoomsAction())
     },[dispatch])
     function filterByDate(dates) {
+        if(dates)
+        {
         setfromdate(moment(dates[0]).format('DD-MM-YYYY'))
-        settodate(moment(dates[1]).format('DD-MM-YYYY'))}
+        settodate(moment(dates[1]).format('DD-MM-YYYY'))
+        var temp=[]
+        for (var room of rooms) {
+          var availability = false;
+          
+          for (var booking of room.currentbookings) {
+            
+            if(room.currentbookings.length)
+            {
+                console.log(!moment(moment(dates[0]).format('DD-MM-YYYY')).isBetween(booking.fromdate, booking.todate))
+                console.log(!moment(moment(dates[1]).format('DD-MM-YYYY')).isBetween(booking.fromdate, booking.todate))
+              if (
+                !moment(moment(dates[0]).format('DD-MM-YYYY')).isBetween(booking.fromdate, booking.todate) &&
+                !moment(moment(dates[1]).format('DD-MM-YYYY')).isBetween(booking.fromdate, booking.todate)
+              ) {
+                if (
+                  moment(dates[0]).format('DD-MM-YYYY') !== booking.fromdate &&
+                  moment(dates[0]).format('DD-MM-YYYY') !== booking.todate &&
+                  moment(dates[1]).format('DD-MM-YYYY') !== booking.fromdate &&
+                  moment(dates[1]).format('DD-MM-YYYY') !== booking.todate
+                ) {
+                  availability = true;
+                }
+              }
+            }
+            
+            
+          }
+          if(availability || room.currentbookings.length==0) 
+          {
+            temp.push(room)
+          }
+          setHotels(temp)
+          setFilterDate(true);
+        }
+    }
+    else
+    {
+        setFilterDate(false)
+    }
+        
+    }
 
     return loading?<Loader></Loader>:error?<Error data="something go wrong"></Error>:success?
     <div className="container">
@@ -27,8 +73,14 @@ export default function HomeScreen() {
           </div>
 
             <div  className="row justify-content-center"> 
-                {success&&(rooms.map(room=>(
+                {!filterDate&&success&&(rooms.map(room=>(
+                    <div key={room._id} className="col-md-9">
+                    {console.log("rooms")}
+                <Room room={room} fromdate={fromdate} todate={todate}></Room>
+                </div>)))}
+                {filterDate&&(hotels.map(room=>(
                 <div key={room._id} className="col-md-9">
+                    {console.log("hotels")}
                 <Room room={room} fromdate={fromdate} todate={todate}></Room>
                 </div>)))}
             </div>
