9ca40029192ba8c54f03436c1e941a218413c18e
Mahmoud Reda Sayed <76118788+MahmoudRedaSayed@users.noreply.github.com>
2022-10-13 15:28:44 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose=require("mongoose")
 
 const ConnectDB = async ()=>{
     try{
-        const con= await mongoose.connect("mongodb+srv://MahmoudReda:01102306392@cluster0.eqhn13z.mongodb.net/Hotel-Booking?retryWrites=true&w=majority",{
+        const con= await mongoose.connect("",{
             useUnifiedTopology:true,
         })
         console.log("the connection is stable");
